192f1651144b2d5eae554b4ec36ee4e95f72187e
Nelson Diaz Rojas <nelsondiazrojas0130@gmail.com>
2020-07-02 22:32:19 -0500

    * Add endpoint to search students users by a string

--- src/main/java/com/adaptativelearning/user/UserController.java
+++ src/main/java/com/adaptativelearning/user/UserController.java
@@ -1,12 +1,17 @@
 package com.adaptativelearning.user;
 
 import com.adaptativelearning.base.BaseController;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +20,9 @@ public class UserController extends BaseController<User, Integer, User, User>
 {
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    UserRepository userRepository;
 
     public UserController()
     {
@@ -26,5 +34,13 @@ public class UserController extends BaseController<User, Integer, User, User>
     {
         requestEntity.setPassword(bCryptPasswordEncoder.encode(requestEntity.getPassword()));
         return super.createResource(requestEntity);
+    }
+
+    @ApiOperation(value = "Find a resource with student role by parameter")
+    @ApiResponse(code = 404, message = "The resource was not found")
+    @GetMapping("/searchStudentUser")
+    public ResponseEntity<List<User>> searchStudentUser(@RequestParam String search)
+    {
+        return ResponseEntity.ok(userRepository.searchStudentUser(search));
     }
 }
--- src/main/java/com/adaptativelearning/user/UserRepository.java
+++ src/main/java/com/adaptativelearning/user/UserRepository.java
@@ -1,6 +1,9 @@
 package com.adaptativelearning.user;
 
 import com.adaptativelearning.base.BaseRepository;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -9,4 +12,14 @@ public interface UserRepository extends BaseRepository<User, Integer>
     User findByIdNumber(Integer idNumber);
 
     Boolean existsByIdNumber(Integer idNumber);
+
+    @Query(
+        value = "SELECT * FROM users u, roles r"
+            + " WHERE (u.id_number like CONCAT('%', :search ,'%')"
+            + " OR u.names like CONCAT('%', :search ,'%')"
+            + " OR u.last_names like CONCAT('%', :search ,'%'))"
+            + " AND u.id_role = r.id"
+            + " AND r.name like '%estudiante%'",
+        nativeQuery = true)
+    List<User> searchStudentUser(@Param("search") String search);
 }
