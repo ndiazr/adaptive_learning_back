637226322a494c3df7f74def7deb642d78c1b341
Nelson Diaz Rojas <nelsondiazrojas0130@gmail.com>
2020-08-02 17:31:14 -0500

    * Add endpoint to find reinforcement by theme

--- src/main/java/com/adaptativelearning/reinforcement/ReinforcementController.java
+++ src/main/java/com/adaptativelearning/reinforcement/ReinforcementController.java
@@ -1,6 +1,13 @@
 package com.adaptativelearning.reinforcement;
 
 import com.adaptativelearning.base.BaseController;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReinforcementController
     extends BaseController<Reinforcement, Integer, Reinforcement, Reinforcement>
 {
+    @Autowired
+    private ReinforcementService reinforcementService;
+
     public ReinforcementController()
     {
         super(Reinforcement.class, Reinforcement.class, Reinforcement.class);
+    }
+
+    @ApiOperation(value = "Find a resource by theme")
+    @ApiResponse(code = 404, message = "The resource was not found")
+    @GetMapping("/findByTheme/{theme}")
+    public ResponseEntity<List<Reinforcement>> findResourceByTheme(@PathVariable(name = "theme") Integer idTheme)
+    {
+        return ResponseEntity.ok(reinforcementService.findByIdTheme(idTheme));
     }
 }
